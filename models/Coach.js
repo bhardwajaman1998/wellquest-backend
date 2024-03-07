aa9163afc48aa524b76ef21a14d3707224357522
Harnoor Kaur <63140074+harnoor497@users.noreply.github.com>
2024-03-06 21:11:46 -0800

    feat: coachSchema change for updated data to add

--- models/Coach.js
+++ models/Coach.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 
 const coachSchema = new mongoose.Schema({
     name: String,
+    bio:String,
     email: String,
     num_of_clients: Number,
     exp: Number,
     completed_goals: Number,
     gyms: [String],
-    description_array: [String],
-    booked_slot: [Date]
+    description_array: [[String]],
+    booked_slot: [Date],
+    available_slots: [[String]]
 });
   
 const Coach = mongoose.model('Coach', coachSchema, 'coach', 'wellquest');
